a14f08c2c88867795032dc44eb00226f2d110cc7
Liza Neiman <5gb.place.for.love@gmail.com>
2023-08-23 19:32:45 +0300

    cleanup

--- src/junglesimulator/_main/_Main.java
+++ src/junglesimulator/_main/_Main.java
@@ -5,9 +5,10 @@ import util.EventProducer;
 
 public class _Main {
     public static void main(String[] args) {
-       Shark shark = new Shark();
-       EventProducer eventProducer = new EventProducer();
+        Shark shark = new Shark();
+        EventProducer eventProducer = new EventProducer();
 
-       eventProducer.startSimulation(shark);
+        eventProducer.startSimulation(shark);
+
     }
 }
